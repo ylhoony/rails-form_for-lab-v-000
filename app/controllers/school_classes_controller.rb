ca8ca8a95d3db2c2d5283ce931304003f0910a1d
Hoon Lee <yl.hoony@gmail.com>
2018-01-24 16:56:01 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -9,7 +9,10 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
+    @school_class = SchoolClass.new(post_params(:title, :room_number))
+    
 
+    redirect_to school_class_path(@school_class)
   end
 
   def show
