7fcf1ec8160199e47ac294a53c49756505178e6b
Hoon Lee <yl.hoony@gmail.com>
2018-01-24 17:00:51 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -10,7 +10,7 @@ class SchoolClassesController < ApplicationController
 
   def create
     @school_class = SchoolClass.new(post_params(:title, :room_number))
-    
+    @school_class.save
 
     redirect_to school_class_path(@school_class)
   end
